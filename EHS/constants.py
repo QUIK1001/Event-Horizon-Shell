b1f2a36975554520374cbadf264ad31b2432acdc
quik <belov0507artem@gmail.com>
2025-09-28 15:22:22 +0300

    v1.2.0

---
+++ EHS/constants.py
@@ -0,0 +1,73 @@
+from colorama import Fore, Style
+
+class Config:
+    SHELL_VERSION = "v1.2.0"
+    GITHUB_LATEST_UPDATE = "https://raw.githubusercontent.com/QUIK1001/Event-Horizon-Shell/main/check_update"
+    CHANGELOG = f"""
+{Fore.RED} What's new in {SHELL_VERSION}? {Style.RESET_ALL}
+{Fore.YELLOW} Archiver:
+  {Fore.GREEN}• Create ZIP archive
+  •  Extract ZIP archive
+  •  List directory contents
+  •  Change directory
+  •  Show current directory
+  •  Back to main menu {Style.RESET_ALL}
+{Fore.YELLOW} Plugin System (Beta):
+  {Fore.GREEN}• The auto-detects plugin function
+  ⠋Create any .py file and move it to the "EHS" folder{Style.RESET_ALL}
+{Fore.YELLOW} Password generator{Style.RESET_ALL}
+"""
+    ENABLE_LOGGING = True
+    API_KEY = "22665d0c08a6c4891dd1cf15717ce820"
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+    THEMES = {
+        "default": {
+            "prompt": Fore.CYAN,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.BLUE,
+            "warning": Fore.YELLOW,
+            "menu": Style.BRIGHT
+        },
+        "dark": {
+            "prompt": Fore.MAGENTA,
+            "error": Fore.YELLOW,
+            "success": Fore.CYAN,
+            "info": Fore.LIGHTBLUE_EX,
+            "warning": Fore.LIGHTYELLOW_EX,
+            "menu": Style.BRIGHT
+        },
+        "light": {
+            "prompt": Fore.BLUE,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.BLACK,
+            "warning": Fore.YELLOW,
+            "menu": Style.NORMAL
+        },
+        "matrix": {
+            "prompt": Fore.GREEN,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.GREEN,
+            "warning": Fore.YELLOW,
+            "menu": Style.BRIGHT
+        }
+    }
+
+    CURRENT_THEME = "default"
+
+    ALIASES = {
+        "ls": "dir",
+        "cls": "clear",
+        "list": "dir",
+        "rm": "rmfile",
+        "del": "rmfile",
+        "delete": "rmfile",
+        "mv": "move",
+        "cp": "copy",
+        "rn": "rename",
+    }
+
+config = Config()
