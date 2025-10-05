d11b84b5abf3b5ad724d67a0a7e1fb94754b86e0
quik <belov0507artem@gmail.com>
2025-10-05 14:56:48 +0300

    v1.2.1

---
+++ EHS/constants.py
@@ -0,0 +1,65 @@
+from colorama import Fore, Style
+
+class Config:
+    SHELL_VERSION = "v1.2.1"
+    GITHUB_LATEST_UPDATE = "https://raw.githubusercontent.com/QUIK1001/Event-Horizon-Shell/main/check_update"
+    CHANGELOG = f"""
+{Fore.RED} What's new in {SHELL_VERSION}? {Style.RESET_ALL}
+{Fore.YELLOW} Shell:
+  {Fore.GREEN}• grep
+  • wc{Style.RESET_ALL}
+"""
+    ENABLE_LOGGING = False
+    API_KEY = "22665d0c08a6c4891dd1cf15717ce820"
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+    THEMES = {
+        "default": {
+            "prompt": Fore.CYAN,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.BLUE,
+            "warning": Fore.YELLOW,
+            "menu": Style.BRIGHT
+        },
+        "dark": {
+            "prompt": Fore.MAGENTA,
+            "error": Fore.YELLOW,
+            "success": Fore.CYAN,
+            "info": Fore.LIGHTBLUE_EX,
+            "warning": Fore.LIGHTYELLOW_EX,
+            "menu": Style.BRIGHT
+        },
+        "light": {
+            "prompt": Fore.BLUE,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.BLACK,
+            "warning": Fore.YELLOW,
+            "menu": Style.NORMAL
+        },
+        "matrix": {
+            "prompt": Fore.GREEN,
+            "error": Fore.RED,
+            "success": Fore.GREEN,
+            "info": Fore.GREEN,
+            "warning": Fore.YELLOW,
+            "menu": Style.BRIGHT
+        }
+    }
+
+    CURRENT_THEME = "default"
+
+    ALIASES = {
+        "ls": "dir",
+        "cls": "clear",
+        "list": "dir",
+        "rm": "rmfile",
+        "del": "rmfile",
+        "delete": "rmfile",
+        "mv": "move",
+        "cp": "copy",
+        "rn": "rename",
+    }
+
+config = Config()
